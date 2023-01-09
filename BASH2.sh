0bdf44dac6d365a22f61baf8c3964b4d9c660236
ilodia <elodie.esteves@sorbonne-nouvelle.fr>
2023-01-09 19:04:20 +0100

    MAJ itrameur

---
+++ BASH2.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+# traitement de l'année 2016, on crée on écrase le fichier sortie.txt
+echo "pour l'année 2016" > sortie.txt
+grep "Location 2016*.ann | wc -l >> sortie.txt
+# traitement de l'année 2017, on écrit à la suite du fichier
+echo "pour l'année 2017" >> sortie.txt
+grep "Location" 2017*.ann | wc -l >> sortie.txt
+#traitement de l'année 2018, on écrit à la suite du fichier
+echo "pour l'année 2018" >> sortie.txt
+grep "Location" 2018*.ann | wc -l >> sortie.txt
+
+annee=$1
+entite=$2
+
+echo "$annee"
+echo "pour l'annee $annee" > sortie.txt
+grep  
+
